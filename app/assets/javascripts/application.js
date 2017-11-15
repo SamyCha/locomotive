c6065145b763e34759f48322a6fd96be27f8f869
Samy Chalakani <samychalakani@gmail.com>
2017-11-15 18:47:12 +0100

    flash user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require toastr
 //= require_tree .
+
+toastr.options = {
+
+ "positionClass": "toast-bottom-right"
+
+ }
