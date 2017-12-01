b39de1100af5e5db240c540db3b67bd8348d1573
Samy Chalakani <samychalakani@gmail.com>
2017-12-01 13:27:25 +0100

    map = OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,23 +5,6 @@ class PagesController < ApplicationController
     @products = Product.order("RANDOM()").where(active: true).limit(3)
   end
 
-    def search
-      if params[:search].present? && params[:search].strip != ""
-         session[:airmoires_search] = params[:search]
-      end
-        arrResult = Array.new
-      if session[:armoires_search] && session[:armoires_search] != ""
-         @products_address = Product.where(active: true).near(session[:armoires_search], 5, order:'distance')
-      else
-         @products_address = Product.where(active: true).all
-      end
-        @search = @products_address.ransack(params[:q])
-        @products = @search.result
-        @arrProducts = @products.to_a
-      #on peut ici ajouter des conditions supplémentaires
-
-      end
-
   def contact
   end
 
