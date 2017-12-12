4a6896edd1d06fe79039d6025becac4497b83851
Samy Chalakani <samychalakani@gmail.com>
2017-12-12 15:04:37 +0100

    retention de l'adresse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo, :avatar, :phone, :description, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo, :avatar, :phone, :address, :description, :email, :password, :password_confirmation])
   end
 end
