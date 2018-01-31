6b7224931a662422a0449cda1722373bb1aa6dfe
Samy Chalakani <samychalakani@gmail.com>
2018-01-31 17:09:06 +0100

    ajeter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,3 +28,33 @@ toastr.options = {
  "positionClass": "toast-bottom-right"
 
  }
+
+
+var search = instantsearch({
+  // Replace with your own values
+  appId: '012967GRTQ',
+  apiKey: 'Ya3f100606d982f64d7c25410c672f5f5', // search only API key, no ADMIN key
+  indexName: 'Product',
+  urlSync: true,
+  searchParameters: {
+    hitsPerPage: 10
+  }
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-input'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: document.getElementById('hit-template').innerHTML,
+      empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
+    }
+  })
+);
+
+search.start();
--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,6 @@
-AlgoliaSearch.configuration = { application_id: '012967GRTQ', api_key: '30cb1e37472e228868705e6625efc35b' }
+require 'rubygems'
+require 'algoliasearch'
+
+Algolia.init application_id: '012967GRTQ',
+             api_key:        '30cb1e37472e228868705e6625efc35b'
+index = Algolia::Index.new("Product")
