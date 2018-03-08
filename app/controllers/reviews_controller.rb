abe971f92f56e37bd5827df5c8f0fb5d5b9b9b8c
Samy Chalakani <samychalakani@gmail.com>
2018-03-08 17:14:59 +0100

    project show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,16 +5,8 @@ class ReviewsController < ApplicationController
 
   def create
     @review = current_user.reviews.new(review_params)
-    if check_password
-      @review.save
-      redirect_to @review.product, alert: "Suite à notre modération, votre commentaire sera publié"
-    else
-      redirect_to @review.product, alert: "Le code est incorrect"
-    end
+    @review.save
   end
-
-
-
 
 def rating
  @reviews = current_user.reviews
@@ -25,12 +17,9 @@ def rating
   private
 
   def review_params
-    params.require(:review).permit(:comment, :star, :product_id, :user_id, :password, :active)
+    params.require(:review).permit(:comment, :star, :product_id, :user_id, :active)
   end
 
-  def check_password
-  @product = Product.find(params[:product_id])
-    return @review.password == @product.price*10
-  end
+
 
 end
